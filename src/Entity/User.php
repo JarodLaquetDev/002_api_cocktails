efef73f1913122813d683fe3cf0665ccafc2a968
Jarod Laquet <jarod.laquet@ynov.com>
2022-11-05 16:00:09 +0100

    Creation_UserListener

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\EntityListeners('App\EntityListener\UserListener')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     // Identifiant unique
