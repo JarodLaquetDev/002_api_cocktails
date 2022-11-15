30e93742861c5b448c25420d9f9e4612658fd364
alexis jeanjean <alexjnj1301>
2022-11-15 10:15:47 +0100

    refreshtoken

--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -5,10 +5,11 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 
-/**
- * @ORM\Entity
- * @ORM\Table("refresh_tokens")
- */
+#[ORM\Entity()]
 class RefreshToken extends BaseRefreshToken
 {
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 }
